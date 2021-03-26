c21f5dd795b3d0cf16cf09d1902bcf9d3e3aac83
Giovanna <giovannagmoeller@gmail.com>
2021-03-26 02:36:36 -0300

    Modifying colors

--- address-book/Views/Colors.swift
+++ address-book/Views/Colors.swift
@@ -10,9 +10,9 @@ import SwiftUI
 
 struct Colors {
     static let background = Color(red: 246/255, green: 246/255, blue: 246/255)
-    static let mainColor = Color(red: 34/255, green: 149/255, blue: 215/255)
-    static let accentColor = Color(red: 51/255, green: 75/255, blue: 95/255)
-    static let shadowColor1 = Color.black.opacity(0.02)
-    static let shadowColor2 = Color.black.opacity(0.03)
-    static let shadowColor3 = Color.black.opacity(0.04)
+    static let main = Color(red: 34/255, green: 149/255, blue: 215/255)
+    static let accent = Color(red: 51/255, green: 75/255, blue: 95/255)
+    static let shadow1 = Color.black.opacity(0.02)
+    static let shadow2 = Color.black.opacity(0.03)
+    static let shadow3 = Color.black.opacity(0.04)
 }
